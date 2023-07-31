6b7804b57e5ee7737ca45b77382acf27325d407a
1a3orn <1a3orn@protonmail.com>
2023-07-31 18:08:06 -0400

    ASD

--- model.py
+++ model.py
@@ -302,7 +302,7 @@ class Retention(nn.Module):
         #print("kr", kr.shape)
         #print("kv", kv.shape)
         if "prev_key_value" in incremental_state:
-            print("using ", kv.shape, kr.shape, v.shape)
+            #print("using ", kv.shape, kr.shape, v.shape)
             prev_kv = incremental_state["prev_key_value"]
             prev_scale = incremental_state["scale"]
             scale = prev_scale * decay + 1
@@ -350,6 +350,7 @@ class Retention(nn.Module):
         q = q.view(bs, leng, heads_num, self.kq_dim).transpose(1, 2)
         k = k.view(bs, leng, heads_num, self.kq_dim).transpose(1, 2)
 
+        print(sin.shape, cos.shape, inner_mask.shape)
         qr = theta_shift(q, sin, cos)
         kr = theta_shift(k, sin, cos)
 
