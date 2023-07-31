645ef247238e134710dc6a7adc22f911a53fe88a
1a3orn <1a3orn@protonmail.com>
2023-07-31 16:44:52 -0400

    ASD

--- model.py
+++ model.py
@@ -616,6 +616,7 @@ class GPT(nn.Module):
 
         incremental_states = {}
         for _ in range(max_new_tokens):
+            print(idx_cond.shape,idx_cond[:,-1:].shape)
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
             logits, _ = self(idx_cond[:,-1:], None, incremental_states)
             logits = logits[:, -1, :] / temperature
