e228cc7031e90f4b48dd137f448e2604e8c33d9a
1a3orn <1a3orn@protonmail.com>
2023-07-31 17:15:32 -0400

    ASD

--- model.py
+++ model.py
@@ -307,7 +307,7 @@ class Retention(nn.Module):
             prev_scale = incremental_state["scale"]
             scale = prev_scale * decay + 1
             #kv = prev_kv * (1 - 1 / scale).view(self.heads_num, 1, 1) + kv / scale.view(self.heads_num, 1, 1)
-            kv = prev_kv * decay.view(self.num_heads, 1, 1) + kv
+            kv = prev_kv * decay.view(self.heads_num, 1, 1) + kv
         else:
             scale = torch.ones_like(decay)
 
