7d0a4d2a8ee3a06bd4cd8fa62bd9a412c6eb41a1
1a3orn <1a3orn@protonmail.com>
2023-07-31 16:21:22 -0400

    ASD

--- model.py
+++ model.py
@@ -613,7 +613,18 @@ class GPT(nn.Module):
         incremental_states = {}
         for _ in range(max_new_tokens):
             idx_cond = idx
-            logits, _ = self(idx_cond, incremental_states)
+            logits, _ = self(idx_cond, None, incremental_states)
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
