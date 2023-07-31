711360430a0749079cd05b8116587d40cf70a5bb
1a3orn <1a3orn@protonmail.com>
2023-07-31 16:47:09 -0400

    ASD

--- model.py
+++ model.py
@@ -618,8 +618,8 @@ class GPT(nn.Module):
         for _ in range(max_new_tokens):
             
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
-            print(idx.shape,idx_cond[:,-1:].shape)
             logits, _ = self(idx_cond[:,-1:], None, incremental_states)
+            print(idx.shape,idx_cond[:,-1:].shape, incremental_states[1]["prev_key_value"])
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
             if top_k is not None:
